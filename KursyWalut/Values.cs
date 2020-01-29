b8387e014ed1622363562bc6edd0a0c8c3d2bf5e
maciejbandyk <maciejbandyk@gmail.com>
2020-01-29 18:40:53 +0100

    Create Values.cs
    Add Values class helper to print values for WPF list.

---
+++ KursyWalut/Values.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KursyWalut
+{
+    public class Values
+    {
+        public Values(string listType, string currency, double min, double max, double average, double standardDeviation)
+        {
+            Type = listType;
+            Currency = currency;
+            Min = min;
+            Max = max;
+            Average = average;
+            StandardDeviation = standardDeviation;
+        }
+
+        public string Type { get; set; }
+        public string Currency { get; set; }
+        public double Min { get; set; }
+        public double Max { get; set; }
+        public double Average { get; set; }
+        public double StandardDeviation { get; set; }
+    }
+}
